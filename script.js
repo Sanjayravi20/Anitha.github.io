b1e6f8776007c192447dc4fa38ff166c94b20e75
Sanjayravi20 <sanjayravi2022@GMAIL.COM>
2026-03-26 09:25:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ window.onload = function(){
 document.querySelector(".hero").style.opacity = "1";
 
 }
+<script>
+function toggleMenu(){
+  document.getElementById("navMenu").classList.toggle("active");
+}
